1825310d127db1f6d763243c6329255688f269ef
MyCoal <pker1066@gmail.com>
2017-11-26 23:17:23 -0800

    no comments

--- Lab08/anagram.cpp
+++ Lab08/anagram.cpp
@@ -1,23 +1,11 @@
 #include <iostream>
 #include <string>
 using namespace std;
-int length_of_line(string input)
-{
-  //Accumulates the amount of characters in a string, excluding whitespace
-  int pre_length(input.length()), length_line(0);
-  for(int i = 0; i < pre_length; i++)
-  {
-    if(isalpha(input[i]))
-    {
-      length_line++;
-    }
-  }
-  return length_line;
-}
-string alphabet_counter(string input, int length, int alphabet_1[26])
+string alphabet_counter(string input, int alphabet_1[26])
 {
   //
   string alphabet_counts("");
+  int length = input.length();
   for(int i = 0; i < length; i++)
   {
     if(isalpha(input[i]))
@@ -46,13 +34,11 @@ int main()
 
   cout << "Enter first string:" << endl;
   getline(cin,input_1);
-  length_1 = length_of_line(input_1);
-  alphabet_counts_1 = alphabet_counter(input_1, length_1, alphabet_1);
+  alphabet_counts_1 = alphabet_counter(input_1, alphabet_1);
 
   cout << "Enter second string:" << endl;
   getline(cin,input_2);
-  length_2 = length_of_line(input_2);
-  alphabet_counts_2 = alphabet_counter(input_2, length_2, alphabet_2);
+  alphabet_counts_2 = alphabet_counter(input_2, alphabet_2);
 
   if(alphabet_counts_1 == alphabet_counts_2)
   {
